6306642602e958430cd903ff84a110894c988ac5
Christian Esswein <c.esswein@gmail.com>
2017-07-13 10:56:24 +0200

    fixed settings boolean cast

--- gemsearch/settings.py
+++ gemsearch/settings.py
@@ -9,4 +9,4 @@ SLACK_API_TOKEN = os.environ.get('SLACK_API_TOKEN')
 
 '''Set to true on windows systems to run linux programs with bash
 '''
-USE_WINDOWS_BASH = os.environ.get('USE_WINDOWS_BASH') or False
+USE_WINDOWS_BASH = (os.environ.get('USE_WINDOWS_BASH') == 'True')
