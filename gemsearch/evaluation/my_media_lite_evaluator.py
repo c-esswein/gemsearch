c0d9cc58d050d8a25c09d42fbefbf8f935c2f1b0
Christian Esswein <c.esswein@gmail.com>
2017-11-07 15:14:48 +0100

    chanaged exec path

--- gemsearch/evaluation/my_media_lite_evaluator.py
+++ gemsearch/evaluation/my_media_lite_evaluator.py
@@ -12,7 +12,7 @@ from gemsearch.utils.proc import execute_cmd
 
 from gemsearch.settings import USE_WINDOWS_BASH
 # PATH_MY_MEDIA_LITE = '../my_media_lite/bin/item_recommendation'
-PATH_MY_MEDIA_LITE = '../my_media_lite_build/item_recommendation.exe'
+PATH_MY_MEDIA_LITE = '../my_media_lite_build/item_recommendation'
 
 
 def writeUserRating(filePath, userTrack):
