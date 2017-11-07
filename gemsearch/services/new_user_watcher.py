263b51979b474286803fe88a9d26692592c9f161
Christian Esswein <c.esswein@gmail.com>
2017-11-07 12:28:26 +0100

    improved logging and messages for embedding service

--- gemsearch/services/new_user_watcher.py
+++ gemsearch/services/new_user_watcher.py
@@ -9,6 +9,8 @@ from gemsearch.embedding.embed_new_users import embedNewUsers
 import time
 from gemsearch.settings import GEMSEARCH_API_KEY, GEMSEARCH_API_URL
 import requests
+from pprint import pprint
+from gemsearch.utils.slack import slack_send_message, slack_error_message
 
 # ---- config ----
 dataDir = 'data/tmp_extend/'
@@ -17,7 +19,7 @@ outDir = 'data/api/'
 # ---- /config ----
 
 logger.info('started new user watcher service with config: %s', {
-    'dataDir': dataDir,
+    'dataDir': dataDir, 
     'outDir': outDir,
 })
 
@@ -29,10 +31,11 @@ while (True):
             logger.info('new users embedded, restart API')
             r = requests.get(str(GEMSEARCH_API_URL) + '/reload_embedding?token=' + str(GEMSEARCH_API_KEY))
             result = r.json()
-            from pprint import pprint
             pprint(result)
+            slack_send_message('api new user embedder included {} new users'.format(embeddedUserCount))
     except Exception as e:
         logger.error('Error: %s', e)
+        slack_error_message('api new user embedder crashed (will not continue): ', e)
         break # TODO: remove?
 
     time.sleep(30)
